43880bfe1a8c8759272f9469920b2746bb886375
Troy Wilson <troy.wilson@lge.com>
2020-05-15 14:26:41 -0500

    Update for initial testing

--- WebRTC-Demo-App/Sources/Config.swift
+++ WebRTC-Demo-App/Sources/Config.swift
@@ -9,9 +9,17 @@
 import Foundation
 
 // Set this to the machine's address which runs the signaling server
-fileprivate let defaultSignalingServerUrl = URL(string: "ws://127.0.0.1:8080")!
+fileprivate let defaultSignalingServerUrl = URL(string: "ws://192.168.4.99:8080")!
 
 // We use Google's public stun servers. For production apps you should deploy your own stun/turn servers.
+let turn_server_username = "ninefingers"
+let turn_server_password = "youhavetoberealistic"
+
+//fileprivate let defaultIceServers = ["turn:34.201.20.59:3478",
+//                                        "credential:turn_server_password",
+//                                        "username:turn_server_username"]
+
+
 fileprivate let defaultIceServers = ["stun:stun.l.google.com:19302",
                                      "stun:stun1.l.google.com:19302",
                                      "stun:stun2.l.google.com:19302",
